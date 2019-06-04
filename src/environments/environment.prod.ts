6b3e86e5d39c9a93e8b7ee6cb6d4757ca2345f2e
irawati <irawati.gole@costartegix.com>
2019-06-04 14:50:15 +0530

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCkTdaujZCa69dD0tb23px7ru-MktVaALU',
+    authDomain: 'oshop-f2b74.firebaseapp.com',
+    databaseURL: 'https://oshop-f2b74.firebaseio.com',
+    projectId: 'oshop-f2b74',
+    storageBucket: 'oshop-f2b74.appspot.com',
+    messagingSenderId: '367428650943'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCkTdaujZCa69dD0tb23px7ru-MktVaALU',
+    authDomain: 'oshop-f2b74.firebaseapp.com',
+    databaseURL: 'https://oshop-f2b74.firebaseio.com',
+    projectId: 'oshop-f2b74',
+    storageBucket: 'oshop-f2b74.appspot.com',
+    messagingSenderId: '367428650943'
+  }
 };
 
 /*
